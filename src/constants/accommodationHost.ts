b30c435166856d8dd8a2c50419bbcc26ebf9d8fe
tkmktkhr <mukku.wordpress@gmail.com>
2021-04-26 22:45:38 +0900

    feat: add accommodation host

---
+++ src/constants/accommodationHost.ts
@@ -0,0 +1,8 @@
+export const ACCOMMODATION_HOST = {
+  AIRBNB: "airbnb",
+  HAFH: "hafh",
+  NOW_ROOM: "nowRoom",
+  ADDRESS: "nowRoom",
+  ALL: null,
+} as const;
+export type TAccommodationHost = keyof typeof ACCOMMODATION_HOSTS;
